5e9e4e9c8ece786a10848908c8c7aa5b2021fc74
Tianze Jiang <jtz2003@qq.com>
2025-08-11 22:37:39 -0400

    python -> python 3

--- mup_examples/mutransfer_lr_shakespeare_char/mup_moe/run.sh
+++ mup_examples/mutransfer_lr_shakespeare_char/mup_moe/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-# muP hyperparameter transfer with MOE
+# muP hyperparameter transfer with MOE - Bash version
 
-for width in 128 256 512 1024
+for width in 128
 do
-    for lr in 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625 0.001953125 0.0009765625 0.00048828125 0.000244140625 0.0001220703125 0.00006103515625
+    for num_exp in 4 2
     do
-        for num_exp in 4 8 16
+        for lr in 0.125 0.03125 0.0078125 0.001953125 0.00048828125 0.0001220703125
         do
-            for seed in 1 2 3
+            for seed in 1
             do
                 head_size=64
                 n_heads=$((width / head_size))
                 mup_base_width=256
                 mup_width_multiplier=$(echo "scale=8; $width/$mup_base_width" | bc -l)
-                num_act=2  # top-k experts
+                num_act=$((num_exp / 2))  # top-k experts
                 out_dir="mup_examples/mutransfer_lr_shakespeare_char/mup_moe/out/width${width}_depth2_experts${num_exp}_active${num_act}_seed${seed}_lr${lr}"
-                python train.py \
+                python3 train.py \
                     --out_dir=$out_dir \
                     --eval_interval=1 \
                     --log_interval=1 \
@@ -28,18 +28,18 @@ do
                     --wandb_log=False \
                     --csv_log=True \
                     --dataset='shakespeare_char' \
-                    --gradient_accumulation_steps=8 \
-                    --batch_size=1 \
+                    --gradient_accumulation_steps=$((8 * num_exp)) \
+                    --batch_size=64 \
                     --block_size=1024 \
-                    --n_layer=2 \
+                    --n_layer=4 \
                     --n_head=$n_heads \
                     --n_embd=$width \
                     --dropout=0.0 \
                     --bias=False \
                     --init_std=0.02 \
                     --learning_rate=$lr \
-                    --max_iters=122 \
-                    --weight_decay=1e-1 \
+                    --max_iters=300 \
+                    --weight_decay=1e-3 \
                     --beta1=0.9 \
                     --beta2=0.95 \
                     --grad_clip=1.0 \
@@ -52,10 +52,14 @@ do
                     --num_act=$num_act \
                     --moe_tau=1.0 \
                     --moe_bias_lr=1e-2 \
+                    --moe_bias_momentum=0.5 \
+                    --moe_bias_momentum_enabled=True \
+                    --moe_load_balance_method='bias' \
+                    --moe_aux_loss_weight=1.0 \
                     --seed=$seed \
                     --backend='nccl' \
                     --device='cuda' \
-                    --dtype='float32' \
+                    --dtype='float16' \
                     --compile=False
             done
         done
