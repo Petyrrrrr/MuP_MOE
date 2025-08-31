c140b3036bb83f4b930f3f989edeb63b0fae5878
Petyrrrrr <jtz2003@qq.com>
2025-08-31 07:30:44 +0000

    no lr schedule

--- utils.py
+++ utils.py
@@ -94,10 +94,11 @@ def estimate_loss(model, eval_iters, skip_val_loss, get_batch_fn, ctx, collect_m
     return out
 
 def get_lr(it, learning_rate, warmup_iters, lr_decay_iters, min_lr):
-    if it <= warmup_iters:
-        return learning_rate * it / warmup_iters
-    if it > lr_decay_iters:
-        return min_lr
-    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
-    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
-    return min_lr + coeff * (learning_rate - min_lr)
+    return learning_rate
+    # if it <= warmup_iters:
+    #     return learning_rate * it / warmup_iters
+    # if it > lr_decay_iters:
+    #     return min_lr
+    # decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
+    # coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    # return min_lr + coeff * (learning_rate - min_lr)
